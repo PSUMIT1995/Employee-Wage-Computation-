e29a756211f426578c759749dd738a0de55210c5
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-20 19:02:31 +0530

    Uc6 : Calculate Wages till condition of max working hrs or days reached

--- Employee wage computation/Employee wage computation/EmployeeWage.cs
+++ Employee wage computation/Employee wage computation/EmployeeWage.cs
@@ -15,12 +15,14 @@ namespace Employee_wage_computation
             int IS_ABSENT = 2;
             int NUM_OF_WORKING_DAYS = 20;
             int EMP_RATE_PER_HOUR = 20;
+            int MAX_HRS_IN_MONTH = 100;
 
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpwage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -38,11 +40,11 @@ namespace Employee_wage_computation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpwage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
-            Console.WriteLine("Total Emp Wage : " + totalEmpwage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
