f434fdc6bf931ae13a44237c7b8b9c7bab09f3cd
PSUMIT1995 <psumit191.sp@gmail.com>
2022-06-03 19:13:27 +0530

    UC14 : Displaying Total Wages for given Company

--- Employee wage computation/Employee wage computation/Program.cs
+++ Employee wage computation/Employee wage computation/Program.cs
@@ -1,17 +1,57 @@
-﻿
+﻿using System;
+using Employee_wage_computation;
 namespace Employee_wage_computation
 {
     public class Program
     {
-        static void Main(String[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Computation");
+            bool Continue = true;
             //creating object for each company
-            EmpWageBuildArray empWageBuilder = new EmpWageBuildArray();
-            empWageBuilder.addCompanyEmpWage("PVR", 20, 10, 10);
-            empWageBuilder.addCompanyEmpWage("HCL", 20, 10, 10);
-            empWageBuilder.computeEmpWage();
+            EmpWageBuildArray employeeWage = new EmpWageBuildArray();
+
+            while (Continue)
+            {
+                Console.WriteLine("Enter 1 to enter detail or 2 to display emp wage: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        GetDetail(employeeWage);
+                        break;
+                    case 2:
+                        Console.WriteLine("enter company name to get total wage:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine(employeeWage.GetEmployeeWage(name));
+                        break;
+                    default:
+                        Continue = false;
+                        break;
+                }
+            }
+            Console.Read();
+
+
+            static void GetDetail(EmpWageBuildArray employee)
+            {
+                Console.WriteLine("Enter company name:");
+                string companyName = Console.ReadLine();
+                Console.WriteLine("Enter Rate per Hr:");
+                int rate = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter max working days:");
+                int maxDays = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter max working hr:");
+                int maxHr = Convert.ToInt32(Console.ReadLine());
+                employee.addDetail(companyName, rate, maxDays, maxHr);
+                employee.ComputeWage();
+            }
         }
+
     }
 }
+
+
+
+
 
