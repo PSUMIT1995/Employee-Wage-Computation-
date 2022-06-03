2a03471267793757dcdc0a481a2126310650e769
PSUMIT1995 <psumit191.sp@gmail.com>
2022-06-03 18:27:35 +0530

    UC10 : Managing Employee Wages for Multiple Companies

--- Employee wage computation/Employee wage computation/Program.cs
+++ Employee wage computation/Employee wage computation/Program.cs
@@ -3,17 +3,13 @@ namespace Employee_wage_computation
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to the Employee Wage Computation Problem");
-            EmployeeWage WALLMart = new EmployeeWage("WALLMart", 20, 20, 100);
-            EmployeeWage PVR = new EmployeeWage("PVR", 25, 24, 160);
-            WALLMart.computeEmpWage();
-            Console.WriteLine(WALLMart.toString());
-            PVR.computeEmpWage();
-            Console.WriteLine(PVR.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("PVR", 20, 10, 10);
+            empWageBuilder.addCompanyEmpWage("HCL", 20, 30, 10);
+            empWageBuilder.computeEmpWage();
         }
     }
-
 }
 
